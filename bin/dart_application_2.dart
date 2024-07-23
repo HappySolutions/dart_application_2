c7b4a705428873eb09f39f078f76967447084baa
HappySolutions <40526308+HappySolutions@users.noreply.github.com>
2024-07-23 17:18:51 +0300

    Update dart_application_2.dart

--- bin/dart_application_2.dart
+++ bin/dart_application_2.dart
@@ -5,18 +5,24 @@ void main() {
 }
 
 String longestCommonPrefix(List<String> strs) {
+  //initialize the returned string
   var result = "";
+  //sort the list in ascending order so the strs[0] will be the shortest in length
   strs.sort((a, b) => a.length.compareTo(b.length));
   print(strs);
+  
   for (var i = 0; i < strs[0].length; i++) {
+    //save the value of the first letter to compare in other strings
     var x = strs[0][i];
     for (var j = 0; j < strs.length; j++) {
+      //the first not equal string means that all the previous are equal so will stop the loop and return the prev prifix
       if (strs[j][i] != x) {
         result = strs[0].substring(0, i);
         print(result);
         return result;
       }
-      result = strs[0].substring(0, strs[0].length);
+      //if the condition didn't match this means the first word in the list is the longest common prefix
+      result = strs[0];
     }
   }
   print(result);
